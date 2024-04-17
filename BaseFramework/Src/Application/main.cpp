e44e8277b952e1d88b4c775081174e5ca98823bf
Genko1204019 <kd1387464@st.kobedenshi.ac.jp>
2024-04-17 10:52:02 +0900

    Debug
    delete m_test

--- BaseFramework/Src/Application/main.cpp
+++ BaseFramework/Src/Application/main.cpp
@@ -221,8 +221,7 @@ bool Application::Init(int w, int h)
 	//===================================================================
 	KdAudioManager::Instance().Init();
 
-	//テスト用　消す
-	m_test = 99999999999;
+	
 
 
 	return true;
